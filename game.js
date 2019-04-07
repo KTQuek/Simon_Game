6777f33eaf088790a0b225eb2950f8a7d24c2d52
KTQuek <qkt7777@gmail.com>
2019-04-07 13:48:25 +0800

    Game start logic added

--- game.js
+++ game.js
@@ -1,9 +1,18 @@
 
 var buttonColours = ["red", "blue", "green", "yellow"];
-
 var gamePattern = [];
+var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-var userClickedPattern = []
+$(document).keypress(function() {
+
+  if(!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
 
 function playSound(colorChosen) {
@@ -16,10 +25,35 @@ $(".btn").click(function() {
   var userChosenColour = $(this).attr("id");
   userClickedPattern.push(userChosenColour);
   playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length - 1);
 
 });
 
+
+function checkAnswer(currentLevel) {
+
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    console.log("wrong");
+  }
+}
+
 function nextSequence() {
+
+  userClickedPattern = [];
+
+  level++;
+  $("#level-title").text("Level " + level);
+
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
@@ -28,4 +62,14 @@ function nextSequence() {
   $(elementChosen).fadeIn(100).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColour);
+}
+
+function animatePress(currentColor){
+
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+
 }
