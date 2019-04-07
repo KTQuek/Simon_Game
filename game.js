fa6492d24957b62bb37cadef3144a183385ed7b3
KTQuek <qkt7777@gmail.com>
2019-04-07 14:48:16 +0800

    Completed

--- game.js
+++ game.js
@@ -35,7 +35,6 @@ $(".btn").click(function() {
 function checkAnswer(currentLevel) {
 
   if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
-    console.log("success");
 
     if (userClickedPattern.length === gamePattern.length) {
       setTimeout(function() {
@@ -44,32 +43,43 @@ function checkAnswer(currentLevel) {
     }
   } else {
     console.log("wrong");
+    playSound("wrong");
+
+    $("body").addClass("game-over");
+
+    setTimeout(function() {
+      $("body").removeClass("game-over");
+    }, 200);
+
+    $("#level-title").text("Game over, Press Any Key to Restart");
+
+    startOver();
   }
 }
 
 function nextSequence() {
 
   userClickedPattern = [];
-
   level++;
   $("#level-title").text("Level " + level);
-
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
 
   var elementChosen = "#" + randomChosenColour;
   $(elementChosen).fadeIn(100).fadeOut(100).fadeIn(100);
-
   playSound(randomChosenColour);
 }
 
 function animatePress(currentColor){
-
   $("#" + currentColor).addClass("pressed");
-
   setTimeout(function () {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
+}
 
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
 }
